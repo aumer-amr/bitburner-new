7c3b46e172a7ebeb5c248f7bb3ee0e10e3b30788
Aumer <aumer@aumer.dev>
2025-04-06 16:05:49 +0200

    fix: double react on global

--- packages/bitburner/global.d.ts
+++ packages/bitburner/global.d.ts
@@ -1,10 +1,8 @@
 /* eslint-disable no-var */
 import { GlobalOverseer } from './servers/home/overseer';
 import { NS as _NS } from 'NetscriptDefinitions';
-import ReactNamespace from 'react/index';
 
 declare global {
 	type NS = _NS;
 	var overseer: GlobalOverseer;
-	const React: ReactNamespace;
 }
